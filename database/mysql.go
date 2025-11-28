a8fccfc02705d88c0d6461129aa88621259c9d0f
zhangyuan <zhangyuan@novgate.com>
2025-11-28 15:38:59 +0800

    init

--- database/mysql.go
+++ database/mysql.go
@@ -7,6 +7,7 @@ import (
 	"github.com/XingMenTech/common/logger"
 	"github.com/beego/beego/v2/client/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/go-querystring/query"
 )
 
 // ------------------[mysql]-------------------
@@ -18,14 +19,36 @@ type MysqlConfig struct {
 	Password         string `yaml:"db_pwd" json:"password" comment:"数据库连接用户名"`
 	Host             string `yaml:"db_host" json:"host" comment:"数据库IP（域名）"`
 	Port             string `yaml:"db_port" json:"port" comment:"数据库端口"`
-	Charset          string `yaml:"db_charset" json:"charset" comment:"字符集类型"`
 	DefaultRowsLimit int    `yaml:"default_rows_limit" json:"defaultRowsLimit" comment:"搜索最大条数限制,-1不限制"`
 	Debug            string `yaml:"db_debug" json:"debug" comment:"是否调试模式"`
 	TablePrefix      string `yaml:"db_table_prefix" json:"tablePrefix" comment:"表前缀"`
+	Charset          string `yaml:"db_charset,omitempty" json:"charset" comment:"字符集类型"`
+	Location         string `yaml:"db_location,omitempty" json:"timeLocation" comment:"时区"`
+}
+
+type LinkParam struct {
+	Loc       string `url:"loc"`
+	Charset   string `url:"charset"`
+	ParseTime bool   `url:"parseTime"`
 }
 
 func (c *MysqlConfig) Url() string {
-	path := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&loc=Local", c.User, c.Password, c.Host, c.Port, c.Name, c.Charset)
+
+	if c.Location == "" {
+		c.Location = "Local"
+	}
+	if c.Charset == "" {
+		c.Charset = "utf8mb4"
+	}
+	linkParam := LinkParam{
+		Loc:       c.Location,
+		Charset:   c.Charset,
+		ParseTime: true,
+	}
+
+	values, _ := query.Values(linkParam)
+
+	path := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", c.User, c.Password, c.Host, c.Port, c.Name, values.Encode())
 	logger.LOG.Debugf("数据库链接：%s \n", path)
 	return path
 }
--- utils/orm_tx_utils.go
+++ utils/orm_tx_utils.go
@@ -1,23 +1,34 @@
 package utils
 
 import (
+	"database/sql/driver"
+
 	"github.com/beego/beego/v2/client/orm"
 )
 
 type TxFunc func(o orm.TxOrmer) error
 
 type OrmTx struct {
-	o orm.TxOrmer
+	o   orm.TxOrmer
+	err error
 }
 
 func NewOrmTx() *OrmTx {
-	ormer, _ := orm.NewOrm().Begin()
+	ormer, err := orm.NewOrm().Begin()
 	return &OrmTx{
-		o: ormer,
+		o:   ormer,
+		err: err,
 	}
 }
 
 func (tx *OrmTx) Execute(f TxFunc) error {
+	if tx.err != nil {
+		return tx.err
+	}
+
+	if tx.o == nil {
+		return driver.ErrBadConn
+	}
 	err := f(tx.o)
 	if err != nil {
 		_ = tx.o.Rollback()
